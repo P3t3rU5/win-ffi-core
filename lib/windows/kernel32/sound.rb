ce1b368fc262e77a2cbbb2f31b5b8c93c0c226e7
P3t3rU5 <pedro.megastore@gmail.com>
2013-09-15 15:43:06 +0100

    sound.rb
    Signed-off-by: P3t3rU5 <pedro.megastore@gmail.com>

--- lib/windows/kernel32/sound.rb
+++ lib/windows/kernel32/sound.rb
@@ -5,21 +5,23 @@ module WinFFI
 
       typedef :pointer, :hwaveout
 
-      SND_SYNC        = 0x0000  # play synchronously (default)
-      SND_ASYNC       = 0x0001  # play asynchronously
-      SND_NODEFAULT   = 0x0002  # silence (!default) if sound not found
-      SND_MEMORY      = 0x0004  # pszSound points to a memory file
-      SND_LOOP        = 0x0008  # loop the sound until next sndPlaySound
-      SND_NOSTOP      = 0x0010  # don:t stop any currently playing sound
+      SoundFlags = enum :sound_flags, [
+        :SYNC,        0x0000, # play synchronously (default)
+        :ASYNC,       0x0001, # play asynchronously
+        :NODEFAULT,   0x0002, # silence (!default) if sound not found
+        :MEMORY,      0x0004, # pszSound points to a memory file
+        :LOOP,        0x0008, # loop the sound until next sndPlaySound
+        :NOSTOP,      0x0010, # don:t stop any currently playing sound
 
-      SND_NOWAIT      = 8192    # don:t wait if the driver is busy
-      SND_ALIAS       = 65536   # name is a registry alias
-      SND_ALIAS_ID    = 1114112 # alias is a predefined ID
-      SND_FILENAME    = 131072  # name is file name
-      SND_RESOURCE    = 262148  # name is resource name or atom
+        :NOWAIT,      8192,   # don:t wait if the driver is busy
+        :ALIAS,       65536,  # name is a registry alias
+        :ALIAS_ID,    1114112,# alias is a predefined ID
+        :FILENAME,    131072, # name is file name
+        :RESOURCE,    262148, # name is resource name or atom
 
-      SND_PURGE       = 0x0040  # purge non-static events for task
-      SND_APPLICATION = 0x0080  # look for application specific association
+        :PURGE,       0x0040, # purge non-static events for task
+        :APPLICATION, 0x0080, # look for application specific association
+      ]
 
       ffi_lib 'kernel32'
 
@@ -34,9 +36,9 @@ module WinFFI
       #  LPCTSTR pszSound,
       #  HMODULE hmod,
       #  DWORD fdwSound )
-      attach_function :PlaySound,  [:string, :hmodule, :dword], :bool
-      attach_function :PlaySoundA, [:string, :hmodule, :dword], :bool
-      attach_function :PlaySoundW, [:string, :hmodule, :dword], :bool
+      attach_function :PlaySound,  [:string, :hmodule, SoundFlags], :bool
+      attach_function :PlaySoundA, [:string, :hmodule, SoundFlags], :bool
+      attach_function :PlaySoundW, [:string, :hmodule, SoundFlags], :bool
 
       #MMRESULT waveOutSetVolume(
       #  HWAVEOUT hwo,
